6ba5aba427a9bbf0133ad7cd9e0aa15ff410d864
Thipok Cholsaipant <thipok.ch@gmail.com>
2017-10-31 17:58:24 -0700

    Initial Commit

---
+++ ikid/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ikid
+//
+//  Created by Thipok Cholsaipant on 10/31/17.
+//  Copyright © 2017 Thipok Cholsaipant. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
